ea307e504df92ad7ed3c7f3d7e85f7c731e4ded3
Sanderson Sa <sanderson@redhat.com>
2024-06-17 17:05:51 -0300

    init commit

---
+++ src/main/java/xyz/sandersonsa/kafka_consumer/controller/KafkaController.java
@@ -0,0 +1,20 @@
+package xyz.sandersonsa.kafka_consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    @Autowired
+    private xyz.sandersonsa.kafka_consumer.service.MessageProducer messageProducer;
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam("message") String message) {
+        messageProducer.sendMessage("my-topic", message);
+        return "Message sent: " + message;
+    }
+
+}
---
+++ src/main/java/xyz/sandersonsa/kafka_consumer/service/MessageConsumer.java
@@ -0,0 +1,14 @@
+package xyz.sandersonsa.kafka_consumer.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group-id")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+
+}
