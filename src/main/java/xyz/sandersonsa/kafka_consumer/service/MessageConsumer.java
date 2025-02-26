8a5ded5016c6b3df7fa1a65ff6edcab09df4d688
C1343636 Sanderson Costa De Sa <c1343636@interno.bb.com.br>
2025-02-26 18:11:41 -0300

    bb

--- src/main/java/xyz/sandersonsa/kafka_consumer/service/MessageConsumer.java
+++ src/main/java/xyz/sandersonsa/kafka_consumer/service/MessageConsumer.java
@@ -7,9 +7,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageConsumer {
 
-    @Value( "${spring.kafka.consumer.group-id}" )
-    private String GROUP_ID_CONFIG;
-
     @KafkaListener(topics = "${kafka.consumer.topic}", groupId = "${kafka.consumer.groupid}")
     public void listen(String message) {
         System.out.println("Received message: " + message);
