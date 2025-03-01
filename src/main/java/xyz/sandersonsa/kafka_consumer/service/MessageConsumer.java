574e08e554a325baa2ccedb865c97b959b3ac7c9
C1343636 Sanderson Costa De Sa <c1343636@interno.bb.com.br>
2025-03-01 10:20:43 -0300

    add consumer

--- src/main/java/xyz/sandersonsa/kafka_consumer/service/MessageConsumer.java
+++ src/main/java/xyz/sandersonsa/kafka_consumer/service/MessageConsumer.java
@@ -1,15 +1,18 @@
 package xyz.sandersonsa.kafka_consumer.service;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageConsumer {
 
+    private static Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
+
     @KafkaListener(topics = "${kafka.consumer.topic}", groupId = "${kafka.consumer.groupid}")
     public void listen(String message) {
-        System.out.println("Received message: " + message);
+        logger.info("## RECEIVED MESSAGE: {}\n##ON TOPIC: {}", message, "${kafka.consumer.topic}");
     }
 
 }
